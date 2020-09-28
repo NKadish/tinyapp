8a61c861b1ab8af5cd442967658be0098d165245
NKadish <noahkadish@hotmail.com>
2020-09-28 21:31:10 +0000

    Sends you to a 404 if you put in a /u/<nonexistent small url>

--- express_server.js
+++ express_server.js
@@ -29,27 +29,31 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => { // send the code over to our templates
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => { // for when we want to open the new url page 
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
+app.post("/urls", (req, res) => { // when someone enters a url to be shortened, we generate a random string for it, then put it into the database and then redirect to the page associated
   const newID = generateRandomString();
   urlDatabase[newID] = req.body.longURL;
   res.redirect(`/urls/${newID}`);
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get("/u/:shortURL", (req, res) => { // the actual functionality for using the short urls
   const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  if (longURL === undefined) { // if the url entered does not match one in the object, it 404s
+    res.send('404, the URL you entered was incorrect, please try again');
+  } else { // if it does match it redirects to the page for that url
+    res.redirect(longURL);
+  }
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => { // when they go to this link, it shows them the original url and the small version of it
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
