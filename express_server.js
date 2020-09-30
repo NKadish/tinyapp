24275a061cad3c4d95615b39d565ab0ef2df43c9
NKadish <noahkadish@hotmail.com>
2020-09-30 16:42:21 +0000

    Added a user registration page without the logic yet

--- express_server.js
+++ express_server.js
@@ -15,6 +15,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -79,13 +92,21 @@ app.post('/urls/:shortURL/delete', (req, res) => { // Deletes the selected item 
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
-
+// lets people login and saves their cookies
 app.post('/login', (req, res) => {
   res.cookie('username', req.body.username);
   res.redirect('/urls');
 });
-
+// lets people logout and deletes the cookie
 app.post('/logout', (req, res) => {
   res.clearCookie('username');
   res.redirect('/urls');
 });
+// the registration page
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"], 
+  };
+  res.render("user_reg", templateVars);
+});
+
